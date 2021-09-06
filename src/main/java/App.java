3376a49dd8b3849844dfb567e5c1be8be9bb978c
Thaodoan1234 <thao.doan1@onemount.com>
2021-09-06 14:45:15 +0700

    update commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,9 +10,14 @@ public class App {
 
    //     printString(str);
         isUppercase(str);
+        toTitleCase(str);
+        System.out.println(toTitleCase(str));
+        System.out.println(toTitle(str));
+
 
     }
 
+    //xuất chuỗi
     public static void printString(String str) {
         char kytu;
         for (int i = 0; i < str.length(); i++) {
@@ -21,6 +26,7 @@ public class App {
         }
     }
 
+    // số ký tự in hoa trong chuỗi
     public static void isUppercase(String str) {
         int dem = 0;
         for (int i = 0; i < str.length(); i++) {
@@ -30,5 +36,26 @@ public class App {
         }
         System.out.format("Chuỗi có %d ký tự in  hoa\n", dem);
     }
+
+    // viết hoa chữ cái đầu của từ trong chuỗi
+    public static String toTitleCase(String givenString) {
+        String[] arr = givenString.split(" ");
+        StringBuffer sb = new StringBuffer();
+
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(Character.toUpperCase(arr[i].charAt(0)))
+                    .append(arr[i].substring(1)).append(" ");
+        }
+        return sb.toString().trim();
+    }
+
+    // viết hoa ký tự đầu của chuỗi
+    public static String toTitle(String s) {
+        String s1 = s.substring(0,1).toUpperCase();
+        String sTitle = s1 + s.substring(1);
+        return sTitle;
+    }
+
+
 
 }
